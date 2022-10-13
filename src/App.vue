faeda11fd04da70317c8701a210ee976b55d1829
Lucas <lucaszago.ph@gmail.com>
2022-10-13 21:58:00 +0200

    add scoreboard and form template

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <nav>Signifly Logo</nav>
   </header>
-
   <main>
-    <TheWelcome />
+    <ScoreBoard />
+    <FormComponent />
   </main>
+  <footer></footer>
 </template>
+<script>
+import ScoreBoard from "./components/ScoreBoard.vue";
+import FormComponent from "./components/FormComponent.vue";
+export default {
+  name: "App",
+  components: {
+    ScoreBoard,
+    FormComponent,
+  },
+};
+</script>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  padding: 2rem;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  display: grid;
+  grid-template-columns: 7fr 5fr;
+  padding-inline: 2rem;
+  gap: 1rem;
 }
 </style>
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <section>
+    <div class="tabs">
+      <div class="is--active">Add Team</div>
+      <div>Add Game</div>
+    </div>
+    <div class="form">
+      <div>
+        <label for="team_name">Team Name</label>
+        <input type="text" id="team_name" />
+      </div>
+      <div>
+        <label for="first_name">First Member</label>
+        <input type="text" id="first_name" />
+      </div>
+      <div>
+        <label for="second_name">Second Member</label>
+        <input type="text" id="second_name" />
+      </div>
+      <button>Add Team</button>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "FormComponent",
+};
+</script>
+<style scoped>
+section {
+  background-color: #fff;
+  border-radius: 16px;
+  overflow: hidden;
+}
+.tabs {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0;
+}
+.tabs div {
+  padding: 1.5rem;
+  background-color: #eee;
+}
+.tabs div.is--active {
+  background-color: #fff;
+}
+.form {
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+}
+.form div {
+  display: flex;
+  gap: 0.1rem;
+  flex-direction: column;
+}
+.form button {
+  align-self: flex-end;
+}
+</style>
